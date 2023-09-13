ea685de4b8c3eb675c44a65a85ffd560a78dc5e8
mesadedinheiro <139721799+mesadedinheiro@users.noreply.github.com>
2023-09-13 19:23:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+x = 0;
+y = 0;
+
+drawApple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "O sistema está ouvindo. Por favor, fale.";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "A fala foi reconhecida: " + content; 
+
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+  if(drawApple == "set")
+  {
+    document.getElementById("status").innerHTML = toNumber + " maçãs desenhadas";
+    drawApple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speakData);
+
+    synth.speak(utterThis);
+
+    speakData = "";
+}
